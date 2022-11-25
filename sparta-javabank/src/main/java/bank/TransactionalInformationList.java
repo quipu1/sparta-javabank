2b45221379ba56fe90ff8a45a7febd742d55b8d1
oocone96 <oocone96@gmail.com>
2022-11-25 19:46:51 +0900

    fix TransactionalInformationList getTransaction method

--- sparta-javabank/src/main/java/bank/TransactionalInformationList.java
+++ sparta-javabank/src/main/java/bank/TransactionalInformationList.java
@@ -11,15 +11,15 @@ public class TransactionalInformationList implements Transaction{
     // 앞에서 account의 계좌번호, 소유주, 비밀번호일치 확인 /  해당 account의  거래내역 조회(거래내역 리스트의 계좌번호일치여부만 확인) //
     @Override
     public void getTransaction(String accountNum, String owner, int password) {
-        for (int i=0 ; i < TransactionalInformationList.size(); i++) {
-            TransactionalInformation information = TransactionalInformationList.get(i);
-            if(information.getAccountNum().equals(accountNum)){
-                System.out.println("해당 계좌의 거래내역을 불러옵니다.");
-                System.out.println("거래일자 : " + information.getDate() + " | 거래시간 : " +information.getTime());
-                System.out.println("계좌번호 : " + information.getAccountNum());
-                System.out.println("입금/출금 : " + information.getAvailability());
-                System.out.println("거래금액 : " + information.getTransactionAmount());
-                System.out.println("-------------------------------------");
+        if(TransactionalInformationList.isEmpty()){
+            System.out.println("해당 계좌에는 거래내역이 존재하지 않습니다.");
+        }else{
+            for (int i=0 ; i < TransactionalInformationList.size(); i++) {
+                TransactionalInformation information = TransactionalInformationList.get(i);
+                if(information.getAccountNum().equals(accountNum)){
+                    System.out.println("거래일자 : " + information.getDate() + " | 거래시간 : " +information.getTime() + " | 계좌번호 : " + information.getAccountNum() + " | 입금/출금 : " + information.getAvailability() + " | 거래금액 : " + information.getTransactionAmount()+"원");
+                    System.out.println("------------------------------------------------------------------------------------------------------");
+                }
             }
         }
     }
