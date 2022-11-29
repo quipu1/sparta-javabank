2cdc8829aee64506b4ede363816e62882ca3c585
seungho <55053131+arkt0956@users.noreply.github.com>
2022-11-29 09:59:58 +0900

    Delete sparta-javabank/src/main/java/bank directory

--- sparta-javabank/src/main/java/bank/presentation/LookupInterface.java
+++
@@ -1,8 +0,0 @@
-package bank.presentation;
-public class LookupInterface {
-    public void run() {
-        System.out.println("=====================================================");
-        System.out.println("1 계좌번호로 찾기 | 2. 소유자명으로 찾기");
-        System.out.println("=====================================================");
-    }
-}
